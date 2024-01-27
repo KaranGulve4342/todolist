9e7ffd777a2b67934d370abf0b6a11932eee36c7
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-27 19:49:15 +0530

    Frontend completed

--- app/page.js
+++ app/page.js
@@ -1,15 +1,47 @@
+"use client"
+
 import Todo from "@/Components/Todo";
-import Image from "next/image";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Home() {
+
+  const [formData, setFormData] = useState({
+    title:"",
+    description:"",
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData(form => ({...form, [name]:value}));
+    console.log(formData);
+  }
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      // api code
+
+
+      toast.success('Success');
+
+    } catch(error){
+      // console.error(error);
+      toast.error('Error');
+    }
+  }
+
   return (
     <>
-      <from className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
-        <input type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full" />
-        <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full" />
+      <ToastContainer theme="dark" />
+      <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full" />
+        <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full" />
         <button type="submit" className="bg-orange-600 py-3 px px-11 text-white">Add Todo</button>
-
-      </from>
+      </form>
       
 
       <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
